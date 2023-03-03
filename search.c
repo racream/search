fa18e3d21b9862932ab1bf0d52d2c7f82af8b80f
jakira <jakira@hotmail.com>
2023-03-03 14:32:38 +0800

    search_beta update:arr[0] dont save data

--- search.c
+++ search.c
@@ -17,24 +17,34 @@ typedef struct
     return -1;
 } */
 
-int search(SSTable S,ElemType key){
+/* int search(SSTable S,ElemType key){
     int i = 0;
     while (i < S.len && S.elem[i] != key)
     {
         i++;
     }
     return (S.len == i ? -1 : i);
+} */
+
+int search_beta(SSTable S,ElemType key){
+    S.elem[0] = key;
+    int i = S.len;
+    while (S.elem[0] != S.elem[i])
+    {
+        i--;
+    }
+    return i;
 }
 
 int main(void){
-    ElemType arr[] = {56,23,18,-7,9,72,0,};
-    int len = sizeof(arr)/sizeof(arr[0]);
+    ElemType arr[] = {-1,56,23,18,-7,9,72,0,};
+    int len = sizeof(arr)/sizeof(arr[0])-1;
 
     SSTable S;
     S.elem = arr;
     S.len = len;
-    int result = search(S,-9);
-    if(result == -1){
+    int result = search_beta(S,18);
+    if(result == 0){
         printf("can not found it!\n");
     }else{
         printf("found it at %d\n",result);
